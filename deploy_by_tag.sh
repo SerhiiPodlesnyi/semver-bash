f4f663bcd858288d5ef2030139145836ee178ce1
Underforest <podlesnyiserhii@gmail.com>
2025-01-30 18:33:18 +0200

    Add function _commit_and_push

--- deploy_by_tag.sh
+++ deploy_by_tag.sh
@@ -2,13 +2,14 @@
 
 _get_version(){
   if [ $type == 'helm' ]; then
-    local file_name=$(find $GITHUB_WORKSPACE -iname "Chart.yaml")
+    file_to_update=$(find $GITHUB_WORKSPACE -iname "Chart.yaml")
     echo ${version=$(yq '.version' < $file_name)}
   elif [ $type == 'nodejs' ]; then
-    local file_name=$(find $GITHUB_WORKSPACE -iname "package.json")
+    file_to_update=$(find $GITHUB_WORKSPACE -iname "package.json")
     echo ${version=$(jq '.version' ./package.json | xargs echo)}
   else
-    echo "Current version not found"
+    echo "Unsupported project type: $type"
+    exit 1
   fi
 }
 
@@ -37,11 +38,27 @@ _increment_version() {
   esac
 }
 
+_save_version(){
+  response=$(curl -X POST \
+    -H "Authorization: token $github_token" \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
+    -d "{\"ref\": \"refs/tags/$GITHUB_REF\", \"sha\": \"$(git rev-parse HEAD)\"}")
+
+  # Check the status of the response
+  if echo "$response" | grep -q '"ref":'; then
+    echo "Tag successfully created: $tag"
+  else
+    echo "An error occurred while creating a tag: $response"
+    exit 1
+  fi
+}
+
 _create_tag() {
   response=$(curl -X POST \
     -H "Authorization: token $github_token" \
     -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$repo_owner/$repo_name/git/refs \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
     -d "{\"ref\": \"refs/tags/$tag\", \"sha\": \"$(git rev-parse HEAD)\"}")
 
   # Check the status of the response
@@ -53,8 +70,31 @@ _create_tag() {
   fi
 }
 
+_commit_and_push() {
+  echo "Updated version in $file_to_update to $tag"
+
+  # Commit changes
+  commit_message="Bump version to $tag"
+  response=$(curl -X POST \
+    -H "Authorization: token $github_token" \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$file_to_update \
+    -d "{
+      \"message\": \"$commit_message\",
+      \"content\": \"$(base64 -w 0 < $file_to_update)\",
+      \"sha\": \"$(curl -s -H \"Authorization: token $github_token\" -H \"Accept: application/vnd.github.v3+json\" https://api.github.com/repos/$GITHUB_REPOSITORY/contents/$file_to_update | jq -r '.sha')\"
+    }")
+
+  if echo "$response" | grep -q '"commit"'; then
+    echo "Successfully committed version bump"
+  else
+    echo "Failed to commit changes: $response"
+    exit 1
+  fi
+}
+
 # Checking the availability of parameters
-if [ -z "$repo_owner" ] || [ -z "$repo_name" ] || [ -z "$github_token" ] || [ -z "$type" ] || [ -z "$semver" ]; then
+if [ -z "$github_token" ] || [ -z "$type" ] || [ -z "$semver" ]; then
   echo "One or more parameters are missing. Make sure that all parameters are passed: repo_owner, repo_name, github_token, type, semver"
   exit 1
 fi
@@ -70,4 +110,5 @@ fi
 tag=$(_increment_version)
 echo "New version: $tag"
 
+_commit_and_push
 _create_tag
